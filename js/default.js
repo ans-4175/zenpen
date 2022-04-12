e1ed81e9546695a908214c5b5a3c4ea9a40e747a
Anshorimuslim <ans4175@gmail.com>
2022-04-13 02:16:43 +0700

    fix(default): change default content

--- js/default.js
+++ js/default.js
@@ -1,18 +1,11 @@
-var defaultTitle = 'This is ZenPen';
+var defaultTitle = 'Ini Sebuah Judul';
 var defaultContent = 
 '<p>\
-A minimalist writing zone, where you can block out all distractions and get to what\'s important. The writing!  \
+	Sebuah editor minimalis untuk memudahkan Anda menulis tanpa distraksi dan paling utama, bisa menulis tulisan  \
 </p>\
 <p> \
-	To get started, all you need to do is delete this text (seriously, just highlight it and hit delete), and fill the page with your own fantastic words. You can even change the title!  \
+	Untuk memulai, langsung saja hapus paragraf dan judul ini.  \
 </p> \
 <p> \
-	You can use <b>bold</b>, <i>italics</i>, <b><i>both</i></b> and <a href="http://zenpen.io"> urls </a> just by highlighting the text and selecting them from the tiny options box that appears above it.\
-</p>\
-<blockquote>\
-	Quotes are easy to add too!\
-</blockquote>\
-<p>\
-	If you\'re using ZenPen, and want to contribute a few dollars, there\'s a small donate button on the bottom left.\
-</p>\
-<p>Happy Typing! ~ <b>Tim Holman (@twholman)</b></p>';
+	<i>Highlight</i> teks untuk memunculkan styling editor-nya.\
+<p>Selamat menulis :)</p>';
